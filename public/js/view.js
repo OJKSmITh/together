d3fb56123951fb4a6dd4de62645781c7ada0451b
cloudcoke <lms4296@gmail.com>
2022-11-25 17:05:17 +0900

    feat: view.js 수정 삭제 버튼 구현

--- public/js/view.js
+++ public/js/view.js
@@ -11,3 +11,26 @@ for (let i = 0; i < viewFrm.length; i++) {
   const span = viewFrm[i].querySelector("span");
   span.innerHTML += board[id];
 }
+
+const modifyBtn = document.querySelector("#modify");
+
+const modifyBtnHandler = (e) => {
+  location.href = "/board/modify.html" + idx;
+};
+
+modifyBtn.addEventListener("click", modifyBtnHandler);
+
+const deleteBtn = document.querySelector("#delete");
+
+const deleteBtnHandler = (e) => {
+  boardsObj.splice(index, 1);
+  for (let i = 0; i < boardsObj.length; i++) {
+    boardsObj[i].index = i;
+  }
+
+  const setBoardsStr = JSON.stringify(boardsObj);
+  localStorage.setItem("boards", setBoardsStr);
+  location.href = "/board/list.html";
+};
+
+deleteBtn.addEventListener("click", deleteBtnHandler);
